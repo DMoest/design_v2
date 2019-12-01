9e2b3639abbf945442ecc7ec6d8ebc4137154c77
DMoest <d.andersson86@icloud.com>
2019-12-01 22:32:36 +0100

    Update start theme/style. Update presentation text Kmom02. Refine som responsive stuff. And I think thats Kmom02 done.

--- config/page.php
+++ config/page.php
@@ -19,7 +19,7 @@ return [
             "lang" => "sv",
             "stylesheets" => [
                 // Change here to set another default stylesheet
-                "css/dbwebb-se.min.css",
+                "css/kmom02.css",
                 //"css/kmom01.css",
             ],
             "javascripts" => [
