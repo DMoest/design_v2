d05a55a5d2119ee2b3122af8a5b2f8e406591bae
Daniel Moest <d.andersson86@icloud.com>
2019-12-19 22:08:52 +0100

    Adding KMOM03.less file. Adding V-grid and starting to bulid site from grid.

--- config/page.php
+++ config/page.php
@@ -19,7 +19,7 @@ return [
             "lang" => "sv",
             "stylesheets" => [
                 // Change here to set another default stylesheet
-                "css/kmom02.css",
+                "css/kmom03.css",
                 //"css/kmom01.css",
             ],
             "javascripts" => [
